998fb52823e2ff120cca79b9bb3f83b07d71fd9f
rasedr007 <rashed.wd@gmail.com>
2018-05-23 02:05:20 +0600

    index.php file modified

--- themes/seo-crazy/index.php
+++ themes/seo-crazy/index.php
@@ -14,8 +14,6 @@
 
 get_header(); ?>
 
-
-
     <div class="seo-breadcum">
         <div class="container">
             <div class="row">
@@ -27,7 +25,6 @@ get_header(); ?>
             </div>
         </div>
     </div>
-
     <div class="seo-layout-single-page single-layout-padding">
         <div class="container">
             <div class="row">
@@ -41,36 +38,27 @@ get_header(); ?>
                                 <?php single_post_title(); ?>
                             </h1>
                         </header>
-
                         <?php
                         endif;
-
                         /* Start the Loop */
                         while ( have_posts() ) : the_post();
-
                             /*
                              * Include the Post-Format-specific template for the content.
                              * If you want to override this in a child theme, then include a file
                              * called content-___.php (where ___ is the Post Format name) and that will be used instead.
                              */
                             get_template_part( 'template-parts/content', get_post_format() );
-
                         endwhile;
-
                         the_posts_navigation();
 
                     else :
-
                         get_template_part( 'template-parts/content', 'none' );
-
                     endif; ?>
                 </div>
                 <?php get_sidebar(); ?>
             </div>
         </div>
     </div>
-
     <!-- #primary -->
-
     <?php
 get_footer();
